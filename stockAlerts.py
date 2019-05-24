db2c461951911a108642f09b4db2f19ed0e751bc
Nayan Shah <shah.nayan.m@gmail.com>
2019-05-23 20:02:52 -0700

    Added exception handler

--- stockAlerts.py
+++ stockAlerts.py
@@ -9,7 +9,7 @@ sys.stdout = log
 print ("Start " + str(datetime.datetime.now()))
 
 voice = Voice()
-voice.login("shah.nayan.m@gmail.com", "StockAlerts123")
+voice.login("shah.nayan.m@gmail.com", "StockAlert1234")
 
 round_5mins = 5
 round_10mins = 10
@@ -22,8 +22,8 @@ print("dateTimeNearest10Min " + dateTimeNearest10Min)
 
 apikey = "AORKX5WZHKYNAT1F"
 # apiKeys = ["FQNM", "1BRMFVOI45RA8EFC", "G1YTRRFMBBYDQX6T", "BXLDWMT70TUJFHS4", "LMV6TGJDJQNFQIOX", "CVQ3ZXDM2VSGHLNX"]
-tickers = ["SPY", "QQQ", "AAPL", "TSLA", "XLF", "XLV", "XLU", "SOXX", "XLP", "NFLX", "ABBV"]
-supportResistance = dict({"SPY":"283.26, 284.74, 287.40, 291.24", "QQQ":"181.76, 182.52, 184.43, 186.01", "AAPL":"178.43, 185.47, 193.76, 204.8",  "TSLA":"228.97",  "XLF":"26.33, 26.75, 26.96, 27.17",  "XLV":"87.14, 89.66",  "XLU":"58.25, 58.72",  "SOXX":"162.94, 195.26",  "XLP":"57.25, 57.33",  "NFLX":"348.67, 381.40",  "ABBV":"69.91, 79.72, 82.07, 91.95"})
+tickers = ["SPY", "PFE", "AAPL", "TSLA", "XLF", "XLV", "XLU", "JD", "XLP", "NFLX", "FXI", "AMZN", "BABA", "QCOM"]
+supportResistance = dict({"SPY":"283.26, 284.74, 287.40, 291.24",  "PFE":"41.68, 42.23",  "AAPL":"185.47, 189.03, 191.32, 193.76",  "TSLA":"228.97",  "XLF":"26.33, 26.75, 26.96, 27.17",  "XLV":"87.14, 89.66",  "XLU":"58.25, 58.72",  "JD":"27.66, 28.25, 29.10",  "XLP":"57.25, 57.33",  "NFLX":"348.67, 358.42, 381.40",  "FXI":"40.48, 40.71",  "AMZN":"1778.03, 1993.53",  "BABA":"154.73, 161.95, 163.75",  "QCOM":"72.5, 77.88, 80.38"})
 # "SPY", "TSLA", "UGAZ", "UWTI", "GLD", "BA", "IWM", "QQQ", "VXX", "EWZ", "XLF", "XLV", "XLU", "AAPL", "AMZN", "GOOG", "MSFT", "FB", "NFLX", "NVDA", "TSLA"
 for ticker in tickers:
     print("Working on " + ticker)
@@ -32,25 +32,36 @@ for ticker in tickers:
     if ema9Resp.status_code != 200:
         voice.send_sms(5102697649, "Stock Alerts API not returning ema9Response for "+ticker)
     ema9Data = ema9Resp.json()
-    ema9Now = ema9Data["Technical Analysis: EMA"][dateTimeNearest5Min]["EMA"]
-    print("ema9Now " + ema9Now)
-    ema9Before = ema9Data["Technical Analysis: EMA"][dateTimeNearest10Min]["EMA"]
-    print("ema9Before " + ema9Before)
+    try:
+        ema9Now = ema9Data["Technical Analysis: EMA"][dateTimeNearest5Min]["EMA"]
+        print("ema9Now " + ema9Now)
+        ema9Before = ema9Data["Technical Analysis: EMA"][dateTimeNearest10Min]["EMA"]
+        print("ema9Before " + ema9Before)
+    except Exception as ex:
+        voice.send_sms(5102697649, "ema9 not available for " + ticker)
+        print("ema9 not available for " + ticker)
 
     ema4Resp = requests.get("https://www.alphavantage.co/query?function=EMA&interval=5min&time_period=4&series_type=open&symbol="+ticker+"&apikey="+apikey)
     if ema4Resp.status_code != 200:
         voice.send_sms(5102697649, "Stock Alerts API not returning ema4Response for " + ticker)
     ema4Data = ema4Resp.json()
-    ema4Now = ema4Data["Technical Analysis: EMA"][dateTimeNearest5Min]["EMA"]
-    print("ema4Now " + ema4Now)
-    ema4Before = ema4Data["Technical Analysis: EMA"][dateTimeNearest10Min]["EMA"]
-    print("ema4Before " + ema4Before)
+    try:
+        ema4Now = ema4Data["Technical Analysis: EMA"][dateTimeNearest5Min]["EMA"]
+        print("ema4Now " + ema4Now)
+        ema4Before = ema4Data["Technical Analysis: EMA"][dateTimeNearest10Min]["EMA"]
+        print("ema4Before " + ema4Before)
+    except Exception as ex:
+        voice.send_sms(5102697649, "ema4 not available for " + ticker)
+        print("ema4 not available for " + ticker)
 
-    if ema4Before >= ema9Before and ema4Now < ema9Now:
-        voice.send_sms(5102697649, "SELL " + ticker + ". Levels : " + supportResistance[ticker])
-        print("Sending SELL SMS for " + ticker + ". Levels : " + supportResistance[ticker])
-    if ema4Before <= ema9Before and ema4Now > ema9Now:
-        voice.send_sms(5102697649, "BUY " + ticker + ". Levels : " + supportResistance[ticker])
-        print("Sending BUY SMS for " + ticker + ". Levels : " + supportResistance[ticker])
+    try:
+        if ema4Before >= ema9Before and ema4Now < ema9Now:
+            voice.send_sms(5102697649, "SELL " + ticker + ". Levels : " + supportResistance[ticker])
+            print("Sending SELL SMS for " + ticker + ". Levels : " + supportResistance[ticker])
+        if ema4Before <= ema9Before and ema4Now > ema9Now:
+            voice.send_sms(5102697649, "BUY " + ticker + ". Levels : " + supportResistance[ticker])
+            print("Sending BUY SMS for " + ticker + ". Levels : " + supportResistance[ticker])
+    except NameError:
+        print("One of the EMAs not available for " + ticker)
 
 print ("End " + str(datetime.datetime.now()))
